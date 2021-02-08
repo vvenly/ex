127bdddd2aadb16be9c1d8892783edad99b49111
guowen <867195350@qq.com>
2021-02-08 17:58:06 +0800

    20210208

--- vue-demox/src/components/list/componentFile/l3/draw.vue
+++ vue-demox/src/components/list/componentFile/l3/draw.vue
@@ -2,9 +2,11 @@
   <div>
 
     <h3>拖拽DIV</h3>
-    <div>
+    <div style="position:relative">
         <div v-drag style="width:200px;height:200px;background:#aff;"></div>
-        <vue-drag-resize></vue-drag-resize>
+        <vue-drag-resize style="background: #faf;">
+            <div style="width:80%;height:80%;background:#aff;"></div>
+        </vue-drag-resize>
     </div>
   </div>
 </template>
@@ -22,35 +24,7 @@ export default {
   },
   methods:{
 
-  },
-  directives: {
-        drag: {
-            // 指令的定义
-            bind: function (el) {
-                let odiv = el;   //获取当前元素
-                el.onmousedown = (e) => {
-                    //算出鼠标相对元素的位置
-                    let disX = e.clientX - odiv.offsetLeft;
-                    let disY = e.clientY - odiv.offsetTop;
-                    let left = '';
-                    let top = '';
-                    document.onmousemove = (e)=>{
-                        //用鼠标的位置减去鼠标相对元素的位置，得到元素的位置
-                        left = e.clientX - disX;
-                        top = e.clientY - disY;
-                        //绑定元素位置到positionX和positionY上面
-                        //移动当前元素
-                        odiv.style.left = left + 'px';
-                        odiv.style.top = top + 'px';
-                    };
-                    document.onmouseup = (e) => {
-                        document.onmousemove = null;
-                        document.onmouseup = null;
-                    };
-                };
-            }
-        }
-    }
+  }
 }
 
 
